2da032a7a0dc1c75464661c842e25ee2c36203ef
Darryl Subal <dsubal@att.net>
2023-03-12 16:02:13 -0400

    add variables - list-logi

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,8 +28,36 @@ function generatePassword() {
     );
   }
 
-  // if (!lengthEl) {
-  //   return;
-  // }
+  // numberEl = window.confirm("Do you want to include numbers?");
+
+  // specialEl = window.confirm("Do yo want to include special characters?");
+
+  // lowerEl = window.confirm("Do you want to include lower case letters?");
+
+  // upperEl = window.confirm("Do you want to include upper case letters?");
+
+  // window.alert(`numbers included ${numberEl}`);
+
+  // var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  // // var special = ["!","#","$","%","(",")"];
+
+  // // var letters = ["a","b","c","d","e","f","g","h"]
+
+  // var index = Math.floor(Math.random() * number.length);
+
+  // var choice = number[index];
+
+  // console.log(`var choice = ${choice}`);
+  // console.log(` var index = ${index}`);
+
+  function random_item(items) {
+    return items[Math.floor(Math.random() * lengthEl)];
+  }
+
+  var items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  console.log(random_item(items));
+
+  return_items();
 }
 // the yellow curly brace is for the fnc
