ac0957f93dd15fce19a18f425f2e7b77812cf0be
Darryl Subal <dsubal@att.net>
2023-03-12 12:47:59 -0400

    add files , password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,29 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+//  window.alert(`Your password is xxx`);
+
+// display password on screen.
+// password.innerHTML = `Your password is here`;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // getting and checking length of password
+  lengthEl = window.prompt(
+    "Please choose a password length between 8 and 128 characters. "
+  );
+
+  if (lengthEl < 8 || lengthEl > 128) {
+    window.alert(
+      "Please choose a password length between 8 and 128 characters."
+    );
+  }
+
+  // if (!lengthEl) {
+  //   return;
+  // }
+}
+// the yellow curly brace is for the fnc
